3b99a421b320e2b7aadbc22911298e301f006e5a
drashti <drashtim@zignuts.com>
2023-05-16 19:22:48 +0530

    navbar section created

---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './main.scss'
+import Header from '../components/common/Header'
+
+const Main = () => {
+  return (
+    <div className='homeImg'>
+           <Header/>
+    </div>
+  )
+}
+
+export default Main
